e72409c0b22680e3197868122c3aefcd4f5b153e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-12-11 19:16:53 +0100

    flake8

--- tests/test_setup_keywords.py
+++ tests/test_setup_keywords.py
@@ -16,19 +16,22 @@ class TestSetupKeywords(unittest.TestCase):
         addon1_dir = os.path.join(DATA_DIR, 'setup_reusable_addons', 'addon1')
         subprocess.check_call(['python', 'setup.py', 'egg_info'],
                               cwd=addon1_dir)
-        shutil.rmtree(os.path.join(addon1_dir, 'odoo_addon_addon1.egg-info'))
+        shutil.rmtree(os.path.join(addon1_dir,
+                                   'odoo_addon_addon1.egg-info'))
 
     def test_odoo_addon2(self):
         addon2_dir = os.path.join(DATA_DIR, 'setup_reusable_addons', 'addon2')
         subprocess.check_call(['python', 'setup.py', 'egg_info'],
                               cwd=addon2_dir)
-        shutil.rmtree(os.path.join(addon2_dir, 'odoo_addon_addon2.egg-info'))
+        shutil.rmtree(os.path.join(addon2_dir,
+                                   'odoo_addon_addon2.egg-info'))
 
     def test_custom_project(self):
         project_dir = os.path.join(DATA_DIR, 'setup_custom_project')
         subprocess.check_call(['python', 'setup.py', 'egg_info'],
                               cwd=project_dir)
-        shutil.rmtree(os.path.join(project_dir, 'test_custom_project.egg-info'))
+        shutil.rmtree(os.path.join(project_dir,
+                                   'test_custom_project.egg-info'))
 
 
 if __name__ == '__main__':
