8dc3c62f6dce8680beb4fc1eb66121184b075d7d
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2016-10-01 17:34:30 +0200

    next release will be 1.0.0rc1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 
 setuptools.setup(
     name='setuptools-odoo',
-    version='1.0.0b8.dev0',
+    version='1.0.0rc1.dev0',
     description='A library to help package Odoo addons with setuptools',
     long_description='\n'.join((
         open('README.rst').read(),
