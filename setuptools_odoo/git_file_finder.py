fbd4bb095c3322eac8cece89c82d7b2e01237a91
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-04-24 20:09:11 +0200

    Use our own git file finder.
    Remove dependency on unmaintained setuptools-git.

---
+++ setuptools_odoo/git_file_finder.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2018 ACSONE SA/NV
+# License LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0-standalone.html)
+
+import os
+import subprocess
+
+
+def _git_toplevel(path):
+    try:
+        out = subprocess.check_output([
+            'git', 'rev-parse', '--show-toplevel',
+        ], cwd=(path or '.'), universal_newlines=True)
+        return os.path.realpath(out.strip())
+    except subprocess.CalledProcessError:
+        return None
+
+
+def _git_ls_files_and_dirs(toplevel):
+    out = subprocess.check_output([
+        'git', 'ls-files',
+    ], cwd=toplevel, universal_newlines=True)
+    git_files = {os.path.join(toplevel, f) for f in out.split()}
+    git_dirs = {os.path.dirname(f) for f in git_files}
+    return git_files, git_dirs
+
+
+def find_files(path=''):
+    """ setuptools compatible git file finder that follows symlinks
+
+    Spec here: http://setuptools.readthedocs.io/en/latest/setuptools.html#\
+        adding-support-for-revision-control-systems
+    """
+    toplevel = _git_toplevel(path)
+    if not toplevel:
+        return []
+    git_files, git_dirs = _git_ls_files_and_dirs(toplevel)
+    realpath = os.path.realpath(path)
+    assert realpath.startswith(toplevel)
+    assert realpath in git_dirs
+    seen = set()
+    res = []
+    for dirpath, dirnames, filenames in os.walk(realpath, followlinks=True):
+        realdirpath = os.path.realpath(dirpath)  # resolve symlink
+        if realdirpath not in git_dirs or realdirpath in seen:
+            dirnames[:] = []
+            continue
+        for filename in filenames:
+            fullfilename = os.path.join(dirpath, filename)  # with symlink
+            if os.path.realpath(fullfilename) in git_files:
+                res.append(
+                    os.path.join(path, os.path.relpath(fullfilename, path)))
+        seen.add(realdirpath)
+    return res
---
+++ tests/test_git_file_finder.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2018 ACSONE SA/NV
+# License LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0-standalone.html)
+
+import os
+import subprocess
+
+from pathlib2 import Path
+import pytest
+
+from setuptools_odoo.git_file_finder import find_files
+
+
+@pytest.fixture
+def ingitdir(tmpdir):
+    cwd = os.getcwd()
+    os.chdir(str(tmpdir))
+    try:
+        subprocess.check_call(['git', 'init'])
+        Path('file1').touch()
+        adir = Path('adir')
+        adir.mkdir()
+        (adir / 'filea').touch()
+        bdir = Path('bdir')
+        bdir.mkdir()
+        (bdir / 'fileb').touch()
+        subprocess.check_call(['git', 'add', '.'])
+        yield
+    finally:
+        os.chdir(cwd)
+
+
+def test_basic(ingitdir):
+    assert set(find_files()) == {
+        'file1',
+        'adir/filea',
+        'bdir/fileb',
+    }
+    assert set(find_files('.')) == {
+        './file1',
+        './adir/filea',
+        './bdir/fileb',
+    }
+    assert set(find_files('adir')) == {
+        'adir/filea',
+    }
+
+
+def test_symlinks(ingitdir):
+    (Path('adir') / 'file1link').symlink_to('../file1')
+    (Path('adir') / 'bdirlink').symlink_to('../bdir')
+    subprocess.check_call(['git', 'add', '.'])
+    assert set(find_files('adir')) == {
+        'adir/filea',
+        'adir/file1link',
+        'adir/bdirlink/fileb',
+    }
+
+
+def test_symlink_loop(ingitdir):
+    (Path('adir') / 'loop').symlink_to('../adir')
+    subprocess.check_call(['git', 'add', '.'])
+    assert set(find_files('adir')) == {
+        'adir/filea',
+    }
+
+
+def test_symlink_out_of_git(ingitdir):
+    (Path('adir') / 'outsidefilelink').symlink_to(__file__)
+    (Path('adir') / 'outsidedirlink').symlink_to(Path(__file__) / '..')
+    subprocess.check_call(['git', 'add', '.'])
+    assert set(find_files('adir')) == {
+        'adir/filea',
+    }
