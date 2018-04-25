86c4cbe57c3c1cdc460237a279ae74c4d7a303f9
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-04-25 14:45:18 +0200

    Tentative windows compatibility

--- setuptools_odoo/git_file_finder.py
+++ setuptools_odoo/git_file_finder.py
@@ -20,17 +20,17 @@ def _git_toplevel(path):
 def _git_ls_files_and_dirs(toplevel):
     # use git archive instead of git ls-file to honor
     # export-ignore git attribute
-    prefix = os.path.join(toplevel, '')
-    cmd = ['git', 'archive', '--prefix', prefix, 'HEAD']
+    cmd = ['git', 'archive', '--prefix', toplevel + os.path.sep, 'HEAD']
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=toplevel)
     tf = tarfile.open(fileobj=proc.stdout, mode='r|*')
     git_files = set()
     git_dirs = set([toplevel])
     for member in tf.getmembers():
+        name = member.name.replace('/', os.path.sep)
         if member.type == tarfile.DIRTYPE:
-            git_dirs.add(member.name)
+            git_dirs.add(name)
         else:
-            git_files.add(member.name)
+            git_files.add(name)
     return git_files, git_dirs
 
 
