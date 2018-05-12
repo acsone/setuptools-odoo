48b1437ca6348a84c2307c78c8ccb942bd44f2b5
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-05-12 12:17:19 +0200

    Convert unittest to pytest

--- tests/test_git_postversion.py
+++ tests/test_git_postversion.py
@@ -3,10 +3,9 @@
 # License LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0-standalone.html)
 
 import os
-import shutil
-import tempfile
 import textwrap
-import unittest
+
+import pytest
 
 from setuptools_odoo import git_postversion
 from setuptools_odoo import manifest
@@ -14,61 +13,63 @@ from setuptools_odoo import manifest
 from . import DATA_DIR
 
 
-class TestGitPostversion(unittest.TestCase):
+def test_addon1():
+    """ addon1 has 2 commit after version 8.0.1.0.0 """
+    addon1_dir = os.path.join(DATA_DIR, 'addon1')
+    version = git_postversion.get_git_postversion(addon1_dir)
+    assert version == '8.0.1.0.0.99.dev3'
 
-    def test_addon1(self):
-        """ addon1 has 2 commit after version 8.0.1.0.0 """
-        addon1_dir = os.path.join(DATA_DIR, 'addon1')
-        version = git_postversion.get_git_postversion(addon1_dir)
-        assert version == '8.0.1.0.0.99.dev3'
 
-    def test_addon2(self):
-        """ addon2 has not changed since 8.0.1.0.1 """
-        addon2_dir = os.path.join(DATA_DIR, 'addon2')
+def test_addon2():
+    """ addon2 has not changed since 8.0.1.0.1 """
+    addon2_dir = os.path.join(DATA_DIR, 'addon2')
+    version = git_postversion.get_git_postversion(addon2_dir)
+    assert version == '8.0.1.0.1'
+
+
+def test_addon2_uncommitted_version_change():
+    """ test with a local uncommitted version change """
+    addon2_dir = os.path.join(DATA_DIR, 'addon2')
+    manifest_path = os.path.join(addon2_dir, '__openerp__.py')
+    with open(manifest_path) as f:
+        manifest = f.read()
+    try:
+        with open(manifest_path, "w") as f:
+            f.write(manifest.replace("8.0.1.0.1", "8.0.1.0.2"))
         version = git_postversion.get_git_postversion(addon2_dir)
-        assert version == '8.0.1.0.1'
+        assert version == '8.0.1.0.2.dev1'
+    finally:
+        with open(manifest_path, "w") as f:
+            f.write(manifest)
 
-    def test_addon2_uncommitted_version_change(self):
-        """ test with a local uncommitted version change """
-        addon2_dir = os.path.join(DATA_DIR, 'addon2')
-        manifest_path = os.path.join(addon2_dir, '__openerp__.py')
-        manifest = open(manifest_path).read()
-        try:
-            open(manifest_path, "w").write(manifest.replace("8.0.1.0.1",
-                                                            "8.0.1.0.2"))
-            version = git_postversion.get_git_postversion(addon2_dir)
-            assert version == '8.0.1.0.2.dev1'
-        finally:
-            open(manifest_path, "w").write(manifest)
 
-    def test_addon1_uncommitted_change(self):
-        """ test with a local uncommitted change without version change """
-        addon1_dir = os.path.join(DATA_DIR, 'addon1')
-        manifest_path = os.path.join(addon1_dir, '__openerp__.py')
-        manifest = open(manifest_path).read()
-        try:
-            open(manifest_path, "w").write(manifest.replace("summary",
-                                                            "great summary"))
-            version = git_postversion.get_git_postversion(addon1_dir)
-            assert version == '8.0.1.0.0.99.dev4'
-        finally:
-            open(manifest_path, "w").write(manifest)
+def test_addon1_uncommitted_change():
+    """ test with a local uncommitted change without version change """
+    addon1_dir = os.path.join(DATA_DIR, 'addon1')
+    manifest_path = os.path.join(addon1_dir, '__openerp__.py')
+    with open(manifest_path) as f:
+        manifest = f.read()
+    try:
+        with open(manifest_path, "w") as f:
+            f.write(manifest.replace("summary", "great summary"))
+        version = git_postversion.get_git_postversion(addon1_dir)
+        assert version == '8.0.1.0.0.99.dev4'
+    finally:
+        with open(manifest_path, "w") as f:
+            f.write(manifest)
 
-    def test_no_git(self):
-        """ get version outisde of git repo, get it from manifest """
-        tmp_addon_dir = tempfile.mkdtemp()
-        try:
-            with open(os.path.join(tmp_addon_dir, '__openerp__.py'), 'w') as f:
-                f.write(textwrap.dedent("""\
-                    {
-                       'version': '10.0.1.2.3',
-                    }
-                """))
-            version = git_postversion.get_git_postversion(tmp_addon_dir)
-            assert version == '10.0.1.2.3'
-        finally:
-            shutil.rmtree(tmp_addon_dir)
 
-    def test_no_manifest(self):
-        with self.assertRaises(manifest.NoManifestFound):
-            git_postversion.get_git_postversion(DATA_DIR)
+def test_no_git(tmpdir):
+    """ get version outisde of git repo, get it from manifest """
+    tmpdir.join('__openerp__.py').write(textwrap.dedent("""\
+        {
+           'version': '10.0.1.2.3',
+        }
+    """))
+    version = git_postversion.get_git_postversion(str(tmpdir))
+    assert version == '10.0.1.2.3'
+
+
+def test_no_manifest():
+    with pytest.raises(manifest.NoManifestFound):
+        git_postversion.get_git_postversion(DATA_DIR)
